96a95c7799d60ebb69c480a357cb7f318d608689
Russell Hay <me@russellhay.com>
2019-05-15 13:07:09 -0700

    Fake Async receiver setup

--- src/async.rs
+++ src/async.rs
@@ -1,11 +1,15 @@
 use std::marker::PhantomData;
 use std::net::{ToSocketAddrs, UdpSocket};
 
+pub(super) trait FromRaw<T> {
+    fn from_raw(raw: &[u8]) -> Option<T>;
+}
+
 #[derive(Debug)]
 pub(super) enum AsyncError {
     GeneralError,
     UnableToBind,
-		ReadError,
+    ReadError,
 
     Noncritical(AsyncResumableErrors),
 }
@@ -15,41 +19,69 @@ pub(super) enum AsyncResumableErrors {
     NoData,
 }
 
-pub(super) struct AsyncPacketReceiver<T> {
+pub(super) struct AsyncPacketReceiver<T>
+where
+    T: FromRaw<T>,
+{
     socket: UdpSocket,
-		cache: [u8; 1024],
+    cache: [u8; 1024],
     _type: PhantomData<T>,
 }
 
-impl<T> AsyncPacketReceiver<T> {
+impl<T> AsyncPacketReceiver<T>
+where
+    T: FromRaw<T>,
+{
     pub fn create<A: ToSocketAddrs>(addr: A) -> Result<Self, AsyncError> {
         let socket = UdpSocket::bind(addr).map_err(|_| AsyncError::UnableToBind)?;
 
         Ok(AsyncPacketReceiver {
             socket,
-						cache: [0; 1024],
+            cache: [0; 1024],
             _type: PhantomData::default(),
         })
     }
 
-    pub fn poll(&mut self) -> Result<Self, AsyncError> {
-			let (bytes_read, addr) = self.socket.recv_from(&mut self.cache).map_err(|e| AsyncError::ReadError)?;
-			println!("{}: {:#?}", bytes_read, addr);
-			println!("{:#?}", &self.cache[..bytes_read]);
+		/// Note: This is blocking for now
+    pub fn poll(&mut self) -> Result<T, AsyncError> {
+        let (bytes_read, addr) = self
+            .socket
+            .recv_from(&mut self.cache)
+            .map_err(|e| AsyncError::ReadError)?;
+        println!("{}: {:#?}", bytes_read, addr);
 
-			let slice = &self.cache[..bytes_read];
-			Err(AsyncError::Noncritical(AsyncResumableErrors::NoData))
-		}
+        let slice = &self.cache[..bytes_read];
+        println!("{:?}", slice);
+				T::from_raw(slice).map_or(Err(AsyncError::GeneralError), |s| Ok(s))
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    impl FromRaw<()> for () {
+        fn from_raw(data: &[u8]) -> Option<()> {
+            Some(())
+        }
+    }
+
+    fn send_test_packet() {
+        let socket =
+            UdpSocket::bind("0.0.0.0:6453").expect("Unable to bind to udp socket for send");
+        socket
+            .send_to(
+                &[65, 114, 116, 45, 78, 101, 116, 0, 0, 32, 0, 14, 0, 0],
+                "127.0.0.1:6454",
+            )
+            .expect("Unable to send to 6454");
+    }
+
     #[test]
     fn test_async_recv() {
         let mut v =
             AsyncPacketReceiver::<()>::create("0.0.0.0:6454").expect("Unable to create receiver");
+        send_test_packet();
         let packet = v.poll().expect("");
     }
 }
